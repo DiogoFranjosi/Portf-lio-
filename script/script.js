b8dce25323a364e811647ff5077eaa04cf63e31e
Diogo Franjosi <100142908+DiogoFranjosi@users.noreply.github.com>
2025-04-04 10:54:54 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -16,3 +16,24 @@ function toggleMenu(){
         nav.style.display = 'none';
     }
 }
+
+/* VALIDAÇÃO DO FORMULÁRIO PELO WHATS */
+
+function validateForm(event) {
+    event.preventDefault(); // Impede envio padrão do formulário
+
+    const nome = document.querySelector('input[name="nome"]').value;
+    const email = document.querySelector('input[name="email"]').value;
+    const telefone = document.querySelector('input[name="telefone"]').value;
+    const assunto = document.querySelector('input[name="assunto"]').value;
+    const mensagem = document.querySelector('textarea[name="mensagem"]').value;
+
+    const numeroWhatsApp = '5511977218265';
+
+    const texto = `Olá, me chamo ${nome}%0AEmail: ${email}%0ATelefone: ${telefone}%0AAssunto: ${assunto}%0AMensagem: ${mensagem}`;
+
+    const url = `https://api.whatsapp.com/send?phone=${numeroWhatsApp}&text=${texto}`;
+
+    window.open(url, '_blank'); // Abre o WhatsApp em nova aba
+    return false;
+}
